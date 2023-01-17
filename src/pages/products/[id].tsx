408a82be8f7847ca0d137e08dcc18e51a4a54acc
soprue <soprue@naver.com>
2023-01-17 23:56:51 +0900

    feat : 상품 상세 페이지 생성 및 react-query 캐시 적용

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,3 +1,28 @@
-const ProductDetail = () => <div>상세 페이지</div>
+import { useQuery } from 'react-query'
+import { useParams } from 'react-router-dom'
+import { fetcher, QueryKeys } from '../../queryClient'
+import { Product } from '../../types'
 
-export default ProductDetail;
+import ProductDetail from '../../components/detail'
+
+const ProductDetailPage = () => {
+    const { id } = useParams();
+
+    const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id],
+        () => fetcher({
+            method: "GET",
+            path: `/products/${id}`
+        })
+    );
+
+    if (!data) return null;
+
+    return (
+        <div>
+            <h2>상품 상세</h2>
+            <ProductDetail item={data} />
+        </div>
+    );
+}
+
+export default ProductDetailPage;
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -9,18 +9,9 @@ const ProductList = () => {
         path: "/products"
     }));
 
-    /*
-    category: "men's clothing"
-    description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
-    id: 1
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-    price: 109.95
-    rating: {rate: 3.9, count: 120}
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
-    */
-
     return (
         <div>
+            <h2>상품 목록</h2>
             <ul className="products">
                 { data?.map(product => {
                     return <ProductItem { ...product } key={product.id} />
